d47243dbc70943d4318f536dc713caedbd29fa63
theraffael <55328336+theraffael@users.noreply.github.com>
2024-11-19 11:15:25 +0000

    asdf

--- chatbot/openai_template.py
+++
@@ -1,2 +0,0 @@
-OPENAI_KEY = "Your OpenAI API Key"
-OPENAI_MODEL = "gpt-3.5-turbo-16k"
--- flask_app.py
+++ flask_app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, render_template, request
 
 from chatbot.chatbot import Chatbot
 
-PYTHONANYWHERE_USERNAME = "carvice"
+PYTHONANYWHERE_USERNAME = "kummeraf"
 PYTHONANYWHERE_WEBAPPNAME = "mysite"
 
 app = Flask(__name__)
